da06c7536b2d050c5b3d2959ca29a633de81390b
alejomaar <demonmaar@gmail.com>
2023-01-04 22:50:01 -0500

    ADD Autoreload notebook with src packages

---
+++ notebooks/test.py
@@ -0,0 +1,3 @@
+from features import feat
+
+print('Hello')
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+#python setup.py install
+#python setup.py build
 setup(
     name='src',
     packages=find_packages(),
